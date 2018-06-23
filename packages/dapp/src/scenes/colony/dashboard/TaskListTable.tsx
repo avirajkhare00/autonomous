3f2e71e6182402246a6686c1604ad9c0370cb880
DyslexicMot <tom-kingston@hotmail.com>
2018-06-23 13:51:40 +0100

    dapp/ui/nav cleaned up table

--- packages/dapp/src/scenes/colony/dashboard/TaskListTable.tsx
+++ packages/dapp/src/scenes/colony/dashboard/TaskListTable.tsx
@@ -1,4 +1,5 @@
 import { default as React, SFC } from 'react'
+import { Table } from 'semantic-ui-react'
 import { TaskRow } from './TaskRow'
 import { Task } from '../../../models/Task'
 
@@ -11,22 +12,22 @@ export const TaskListTable: SFC<TaskListTableProps> = ({ tasks, onFinalize }) =>
   <div>
     {tasks.length > 0
       ? (
-        <table>
-          <thead>
-          <tr>
-            <td>TaskId</td>
-            <td>BriefHash</td>
-            <td>Brief</td>
-            <td>ConfigHash</td>
-            <td>Config</td>
-            <td>Finalized</td>
-            <td>Actions</td>
-          </tr>
-          </thead>
-          <tbody>
+        <Table>
+          <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>TaskId</Table.HeaderCell>
+            <Table.HeaderCell>BriefHash</Table.HeaderCell>
+            <Table.HeaderCell>Brief</Table.HeaderCell>
+            <Table.HeaderCell>ConfigHash</Table.HeaderCell>
+            <Table.HeaderCell>Config</Table.HeaderCell>
+            <Table.HeaderCell>Finalized</Table.HeaderCell>
+            <Table.HeaderCell>Actions</Table.HeaderCell>
+          </Table.Row>
+          </Table.Header>
+          <Table.Body>
           {tasks.map((task, i) => <TaskRow task={task} key={i} finalize={() => onFinalize(task.id)}/>)}
-          </tbody>
-        </table>
+          </Table.Body>
+        </Table>
       )
       : <span>No tasks in Colony</span>
     }
--- packages/dapp/src/scenes/colony/dashboard/TaskRow.tsx
+++ packages/dapp/src/scenes/colony/dashboard/TaskRow.tsx
@@ -1,5 +1,5 @@
 import { default as React, SFC } from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Icon, Table } from 'semantic-ui-react'
 
 import { Task } from '../../../models/Task'
 
@@ -10,13 +10,13 @@ interface TaskListTableProps {
 }
 
 export const TaskRow: SFC<TaskListTableProps> = ({ task, finalize }) => (
-  <tr>
-    <td>{task.id}</td>
-    <td>{task.specificationHash.substring(0, 8)}</td>
-    <td>{task.specification.brief}</td>
-    <td>{task.deliverableHash ? task.deliverableHash.substring(0, 8) : ''}</td>
-    <td>{task.deliverable ? task.deliverable.deploymentString : ''}</td>
-    <td>{task.finalized ? 'Yes' : 'No'}</td>
-    <td><Button onClick={() => finalize()}>Finalize</Button></td>
-  </tr>
+  <Table.Row>
+    <Table.Cell>{task.id}</Table.Cell>
+    <Table.Cell>{task.specificationHash.substring(0, 8)}</Table.Cell>
+    <Table.Cell>{task.specification.brief}</Table.Cell>
+    <Table.Cell>{task.deliverableHash ? task.deliverableHash.substring(0, 8) : ''}</Table.Cell>
+    <Table.Cell>{task.deliverable ? task.deliverable.deploymentString : ''}</Table.Cell>
+    <Table.Cell>{task.finalized ? (<div><Icon color='green' name='checkmark' size='large' /></div>) : ''}</Table.Cell>
+    <Table.Cell><Button onClick={() => finalize()}>Finalize</Button></Table.Cell>
+  </Table.Row>
 )
